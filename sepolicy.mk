036174847d3f5b5ae7dbbc08294ee96b65df4bb8
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2015-11-01 16:04:08 +0100

    sepolicy: Include VRToxin+QCOM SEpolicy
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- sepolicy.mk
+++ sepolicy.mk
@@ -5,3 +5,5 @@ BOARD_SEPOLICY_DIRS := \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+
+-include vendor/vrtoxin/sepolicy/qcom/sepolicy.mk
